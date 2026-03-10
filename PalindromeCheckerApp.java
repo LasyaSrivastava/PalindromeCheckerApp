20a54a59f4469ee4c6c94770e02f892d492bd349
LasyaSrivastava <olilasya@gmail.com>
2026-03-10 09:30:16 +0530

    UC1

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+/*
+This class will print the details of the App.
+* @author Lasya Srivastava
+* @Version 1.0
+*
+* */
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized succesfully");
+    }
+}
